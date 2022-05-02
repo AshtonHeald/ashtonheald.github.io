b670dce8e92e46834017b1f549b399535efe3de7
AshtonHeald <devtheash@gmail.com>
2022-05-02 12:55:20 -0500

    removed copyjs and removed/changed some buttons

--- src/js/scripts.js
+++
@@ -1,8 +0,0 @@
-new ClipboardJS(".btn");
-
-function change() {
-	document.getElementById("copy").innerHTML = "Copied!";
-	setTimeout(function () {
-		document.getElementById("copy").innerHTML = "Copy Email";
-	}, 2000);
-}
